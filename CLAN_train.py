111628f7b7abb9b579de92291512bf43128dcf67
omerlandau <omerlan6@gmail.com>
2020-03-06 20:20:28 +0200

    ss

--- CLAN_train.py
+++ CLAN_train.py
@@ -363,11 +363,7 @@ def main():
                           Variable(torch.FloatTensor(D_out.data.size()).fill_(source_label)).cuda(args.gpu))
 
         loss_adv = loss_adv * Lambda_adv * damping
-
-        #loss_norm_target.backward(retain_graph=True)
-
         loss_adv.backward()
-
 
 
         #Weight Discrepancy Loss
@@ -433,12 +429,12 @@ def main():
 
         print('exp = {}'.format(args.snapshot_dir))
         print(
-        'iter = {0:6d}/{1:6d}, loss_seg = {2:.4f} loss_adv = {3:.4f}, loss_weight = {4:.4f}, loss_D_s = {5:.4f} loss_D_t = {6:.4f}'.format(
-            i_iter, args.num_steps, loss_seg, loss_adv, loss_weight, loss_D_s, loss_D_t))
+        'iter = {0:6d}/{1:6d}, loss_seg = {2:.4f} loss_adv = {3:.4f}, loss_weight = {4:.4f}, loss_D_s = {5:.4f} loss_D_t = {6:.4f} loss_norm_src = {7:.4f} loss_norm_target = {8:.4f}'.format(
+            i_iter, args.num_steps, loss_seg, loss_adv, loss_weight, loss_D_s, loss_D_t, loss_norm_src, loss_norm_target))
 
         f_loss = open(osp.join(args.snapshot_dir,'loss.txt'), 'a')
-        f_loss.write('{0:.4f} {1:.4f} {2:.4f} {3:.4f} {4:.4f}\n'.format(
-            loss_seg, loss_adv, loss_weight, loss_D_s, loss_D_t))
+        f_loss.write('{0:.4f} {1:.4f} {2:.4f} {3:.4f} {4:.4f} {5:.4f} {6:.4f}\n'.format(
+            loss_seg, loss_adv, loss_weight, loss_D_s, loss_D_t, loss_norm_src, loss_norm_target))
         f_loss.close()
         
         if i_iter >= args.num_steps_stop - 1:
