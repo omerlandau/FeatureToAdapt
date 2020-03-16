c6a6fce36b6b89a8d677f76a7df71304fa2a88a2
omerlandau <omerlan6@gmail.com>
2020-03-16 21:09:03 +0200

    ss

--- CLAN_train.py
+++ CLAN_train.py
@@ -166,7 +166,7 @@ def discrepancy_slice_wasserstein(p1, p2):
     print(p2.size())
     if list(p1.size())[1] > 1:
         # For data more than one-dimensional, perform multiple random projection to 1-D
-        proj = torch.randn([p1.size()[1], 128]).cuda(args.gpu)
+        proj = torch.randn([p1.size()[1], 128])
         print(proj.size())
         proj *= torch.rsqrt((proj**2).sum(keepdim=True, dim=0)).cuda(args.gpu)
         p1 = torch.matmul(p1, proj)
@@ -391,7 +391,7 @@ def main():
                           Variable(torch.FloatTensor(D_out.data.size()).fill_(source_label)).cuda(args.gpu))
 
         loss_adv = loss_adv * Lambda_adv * damping
-        loss_adv.backward(retain_graph=True)
+        loss_adv.backward()
 
 
         #Weight Discrepancy Loss
