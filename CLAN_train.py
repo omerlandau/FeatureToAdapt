25f67835e3602c5091128b1458028e9d21734bde
omerlandau <omerlan6@gmail.com>
2020-03-16 21:17:56 +0200

    ss

--- CLAN_train.py
+++ CLAN_train.py
@@ -414,6 +414,7 @@ def main():
 
         loss_weight = discrepancy_slice_wasserstein(w5, w6)
         # (torch.matmul(W5, W6) / (torch.norm(W5) * torch.norm(W6)) + 1) # +1 is for a positive loss
+        print(loss_weight)
         loss_weight = -loss_weight * Lambda_weight * damping * 2
         print(loss_weight)
         loss_weight.backward()
