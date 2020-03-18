c1154c1999f2156714a1849586ca001d4d6ff66c
omerlandau <omerlan6@gmail.com>
2020-03-19 00:33:35 +0200

    SEG_LOSS ADDeD + regular weight + norm loss 0.00015 include damping.

--- visualize-datasets.py
+++ visualize-datasets.py
@@ -450,7 +450,7 @@ def main():
 
     gta_images = split_all_imgaes(gta_ids,gta_ids,'GTA',direct_i='./data/GTA5/images', direct_l='./data/GTA5/labels')
 
-    city_images = split_all_imgaes(city_ids_i,city_ids_l,type='city', direct_l='./data/CitySpaces/gtFine/val', direct_i='./data/CitySpaces/leftImg8bit/val')
+    city_images = split_all_imgaes(city_ids_l,city_ids_i,type='city', direct_l='./data/CitySpaces/gtFine/val', direct_i='./data/CitySpaces/leftImg8bit/val')
 
     with open("./GTA_split", 'wb') as pfile:
         pickle.dump(gta_images, pfile, protocol=pickle.HIGHEST_PROTOCOL)
