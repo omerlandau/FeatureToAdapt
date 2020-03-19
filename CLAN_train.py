b2f0946b5cf64b51b0eab025eb0e5d1414af8987
omerlandau <omerlan6@gmail.com>
2020-03-19 20:58:20 +0200

    norm_00015_damping

--- CLAN_train.py
+++ CLAN_train.py
@@ -142,7 +142,7 @@ def get_arguments():
                         help="Where to save snapshots of the model.")
     parser.add_argument("--weight-decay", type=float, default=WEIGHT_DECAY,
                         help="Regularisation parameter for L2-loss.")
-    parser.add_argument("--gpu", type=int, default=2,
+    parser.add_argument("--gpu", type=int, default=0,
                         help="choose gpu device.")
     parser.add_argument("--set", type=str, default=SET,
                         help="choose adaptation set.")
