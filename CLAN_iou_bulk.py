6d479ab69f7f436d2d1e9df75570015756e6b9a6
omerlandau <omerlan6@gmail.com>
2020-03-20 18:43:58 +0200

    norm_00015_damping

--- CLAN_iou_bulk.py
+++ CLAN_iou_bulk.py
@@ -64,9 +64,9 @@ def main(gt_dir, pred_dir, devkit_dir):
 
 
 if __name__ == "__main__":
-    with open("mIoU_result/GTA2Cityscapes_mIoU.csv","a+",newline="") as datacsv:
+    with open("mIoU_result/GTA2Cityscapes_mIoU_002_no_damp.csv","a+",newline="") as datacsv:
         csvwriter = csv.writer(datacsv,dialect = ("excel"))
-        for i in range(1, 50):
+        for i in range(1, 49):
             gt_dir = './data/CitySpaces/gtFine/val'
             pred_dir = './result/GTA2Cityscapes_{0:d}'.format(i*2000)
             devkit_dir = './dataset/cityscapes_list'
