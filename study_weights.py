eb3c65eb91be30dd62d586b655e56bea36a3a353
omerlandau <omerlan6@gmail.com>
2020-03-22 15:59:06 +0200

    study_weights

--- study_weights.py
+++ study_weights.py
@@ -17,6 +17,23 @@ model = Res_Deeplab(num_classes=19)
 saved_state_dict = torch.load(model_path, map_location="cuda:2")
 model.load_state_dict(saved_state_dict)
 
+for (w5, w6) in zip(model.layer5.parameters(), model.layer6.parameters()):
+    if W5 is None and W6 is None:
+        W5 = w5.view(-1)
+        W6 = w6.view(-1)
+    else:
+        W5 = torch.cat((W5, w5.view(-1)), 0)
+        W6 = torch.cat((W6, w6.view(-1)), 0)
+
+print("layer5 weights = {0}\n".format(W5))
+
+print("layer6 weights = {0}\n".format(W6))
+
+
+
+
+
+
 print("Model's state_dict:")
 for param_tensor in model.state_dict():
     if('layer5' in param_tensor.split('.')):
