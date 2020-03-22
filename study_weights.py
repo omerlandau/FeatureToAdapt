e96e6025917462979fb38966565822d42e961f93
omerlandau <omerlan6@gmail.com>
2020-03-22 15:55:19 +0200

    study_weights

--- study_weights.py
+++ study_weights.py
@@ -19,9 +19,6 @@ model.load_state_dict(saved_state_dict)
 
 print("Model's state_dict:")
 for param_tensor in model.state_dict():
-    print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+    if('layer5' in param_tensor.split('.')):
+        print(param_tensor, "\t", model.state_dict()[param_tensor])
 
-# Print optimizer's state_dict
-print("Optimizer's state_dict:")
-for var_name in optimizer.state_dict():
-    print(var_name, "\t", optimizer.state_dict()[var_name])
