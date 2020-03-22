d395300f09a74ab9681519a8a30306ea75415837
omerlandau <omerlan6@gmail.com>
2020-03-23 01:43:59 +0200

    study_weights

--- utils/loss.py
+++ utils/loss.py
@@ -27,8 +27,8 @@ class WeightedBCEWithLogitsLoss(nn.Module):
             raise ValueError("Target size ({}) must be the same as input size ({})".format(target.size(), input.size()))
 
         max_val = (-input).clamp(min=0)
-        print(max_val.shape)
-        print(max_val)
+        print('max_val shape = {0}'.format(max_val.shape))
+        print('max_val = {0}'.format(max_val))
         loss = input - input * target + max_val + ((-max_val).exp() + (-input - max_val).exp()).log()
                 
         if weight is not None:
