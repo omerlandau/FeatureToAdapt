c36513a42c2cd9e0ce17711568882189b35e3a33
omerlandau <omerlan6@gmail.com>
2020-03-23 15:49:52 +0200

    study_weights

--- CLAN_evaluate_bulk.py
+++ CLAN_evaluate_bulk.py
@@ -72,7 +72,7 @@ def get_arguments():
                         help="Number of classes to predict (including background).")
     parser.add_argument("--restore-from", type=str, default=RESTORE_FROM,
                         help="Where restore model parameters from.")
-    parser.add_argument("--gpu", type=int, default=1,
+    parser.add_argument("--gpu", type=int, default=3,
                         help="choose gpu device.")
     parser.add_argument("--set", type=str, default=SET,
                         help="choose evaluation set.")
@@ -84,9 +84,9 @@ def get_arguments():
 def main():
     """Create the model and start the evaluation process."""
 
-    for i in range(20, 32):
-        model_path = './snapshots/GTA2Cityscapes_norm_00015_Damping15_normal_weight_loss_restore_from_40000_G_38_D_numsteps_fixed/GTA5_{0:d}.pth'.format(i*2000)
-        save_path = './result/GTA2Cityscapes_norm_00015_Damping15_normal_weight_loss_restore_from_40000_G_38_D_numsteps_fixed_{0:d}'.format(i*2000)
+    for i in range(1, 15):
+        model_path = './snapshots/GTA2Cityscapes_norm_00015_Damping15_normal_weight_loss_restore_from_40000_G_38_D/GTA5_{0:d}.pth'.format(i*2000)
+        save_path = './result/GTA2Cityscapes_norm_00015_Damping15_normal_weight_loss_restore_from_40000_G_38_D_{0:d}'.format(i*2000)
         args = get_arguments()
     
         gpu0 = args.gpu
