4a10e4d211312bcf311683874b92017d1cffe309
omerlandau <omerlan6@gmail.com>
2020-03-23 19:32:02 +0200

    study_weights

--- CLAN_iou_bulk.py
+++ CLAN_iou_bulk.py
@@ -64,11 +64,11 @@ def main(gt_dir, pred_dir, devkit_dir):
 
 
 if __name__ == "__main__":
-    with open("mIoU_result/GTA2Cityscapes_norm_00015_Damping15_normal_weight_loss_restore_from_40000_G_38_D_numsteps_fixed_mIou.csv","a+",newline="") as datacsv:
+    with open("mIoU_result/GTA2Cityscapes_norm_00012_Damping_power18_012norm_mse_0001_iw.csv","a+",newline="") as datacsv:
         csvwriter = csv.writer(datacsv,dialect = ("excel"))
-        for i in range(20, 32):
+        for i in range(1, 20):
             gt_dir = './data/CitySpaces/gtFine/val'
-            pred_dir = './result/GTA2Cityscapes_norm_00015_Damping15_normal_weight_loss_restore_from_40000_G_38_D_numsteps_fixed_{0:d}'.format(i*2000)
+            pred_dir = './result/GTA2Cityscapes_norm_00012_Damping_power18_012norm_mse_0001_iw_{0:d}'.format(i*2000)
             devkit_dir = './dataset/cityscapes_list'
             mIoU = main(gt_dir, pred_dir, devkit_dir)
             csvwriter.writerow([mIoU])
