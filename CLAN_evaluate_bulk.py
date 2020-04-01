34c1b4d024b4414a8920932ff03477a79896dae3
omerlandau <omerlan6@gmail.com>
2020-04-01 18:13:26 +0300

    revert to normal

--- CLAN_evaluate_bulk.py
+++ CLAN_evaluate_bulk.py
@@ -72,7 +72,7 @@ def get_arguments():
                         help="Number of classes to predict (including background).")
     parser.add_argument("--restore-from", type=str, default=RESTORE_FROM,
                         help="Where restore model parameters from.")
-    parser.add_argument("--gpu", type=int, default=3,
+    parser.add_argument("--gpu", type=int, default=2,
                         help="choose gpu device.")
     parser.add_argument("--set", type=str, default=SET,
                         help="choose evaluation set.")
@@ -84,9 +84,9 @@ def get_arguments():
 def main():
     """Create the model and start the evaluation process."""
 
-    for i in range(22, 29):
-        model_path = './snapshots/GTA2Cityscapes_norm_layer3_00015_18power_damp/GTA5_{0:d}.pth'.format(i*2000)
-        save_path = './result/GTA2Cityscapes_norm_layer3_00015_18power_damp_{0:d}'.format(i*2000)
+    for i in range(1, 51):
+        model_path = './snapshots/GTA2Cityscapes_only_norm/GTA5_{0:d}.pth'.format(i*2000)
+        save_path = './result/GTA2Cityscapes_only_norm_{0:d}'.format(i*2000)
         args = get_arguments()
     
         gpu0 = args.gpu
