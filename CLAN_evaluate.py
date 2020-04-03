4b90a01f1f35ccc40f4211eeb93d9592d497c1cf
omerlandau <omerlan6@gmail.com>
2020-04-03 15:30:41 +0300

    revert to normal

--- CLAN_evaluate.py
+++ CLAN_evaluate.py
@@ -117,6 +117,8 @@ def main():
             #output = np.asarray(np.argmax(output, axis=2), dtype=np.uint8)
 
             output = np.asarray(norm_dims.cpu(), dtype=np.uint8)
+
+            output = output.reshape((output.shape[2],output.shape[3]))
     
             #output_col = colorize_mask(output)
             output = Image.fromarray(output)
