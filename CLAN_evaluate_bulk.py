1f70f11de46e74d34e0de9db96b92cb6ff708e45
omerlandau <omerlan6@gmail.com>
2020-04-07 18:16:38 +0300

    minent_multi + norm + larger

--- CLAN_evaluate_bulk.py
+++ CLAN_evaluate_bulk.py
@@ -84,9 +84,9 @@ def get_arguments():
 def main():
     """Create the model and start the evaluation process."""
 
-    for i in range(1, 51):
-        model_path = './snapshots/GTA2Cityscapes_only_iw_norm/GTA5_{0:d}.pth'.format(i*2000)
-        save_path = './result/GTA2Cityscapes_only_iw_norm_{0:d}'.format(i*2000)
+    for i in range(1, 28):
+        model_path = './snapshots/GTA2Cityscapes_norm_min_ent_multi_0015_0001/GTA5_{0:d}.pth'.format(i*2000)
+        save_path = './result/GTA2Cityscapes_norm_min_ent_multi_0015_0001_{0:d}'.format(i*2000)
         args = get_arguments()
     
         gpu0 = args.gpu
