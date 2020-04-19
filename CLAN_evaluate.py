6fe7debbd5e6bef83d928c7d0e60db176422d3ce
omerlandau <omerlan6@gmail.com>
2020-04-19 17:12:33 +0300

    adding multi option to evaluate

--- CLAN_evaluate.py
+++ CLAN_evaluate.py
@@ -99,6 +99,7 @@ def main():
     else:
         saved_state_dict = torch.load(args.restore_from, map_location="cuda:{0}".format(args.gpu))
         saved_state_dict_2 = torch.load(args.restore_from_second, map_location="cuda:{0}".format(args.gpu))
+        saved_state_dict_2 = saved_state_dict_2["state_dict"]
 
     model.load_state_dict(saved_state_dict)
     model2.load_state_dict(saved_state_dict_2)
