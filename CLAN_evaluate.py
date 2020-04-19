5837b540d4d3b1226899ca438f212607cdf949e0
omerlandau <omerlan6@gmail.com>
2020-04-20 00:02:00 +0300

    adding flip

--- CLAN_evaluate.py
+++ CLAN_evaluate.py
@@ -186,7 +186,7 @@ def main():
                 pred_c = (pred_P + pred_P_2) / 2
                 output_f_2 = pred_c.data.cpu().numpy()
 
-            output_final = torch.Tensor(output_f).cuda(gpu0)*0.25 + (output_f_2).cuda(gpu0)*0.75
+            output_final = torch.Tensor(output_f).cuda(gpu0)*0.25 + torch.Tensor(output_f_2).cuda(gpu0)*0.75
 
             output = interp(output_final).cpu().data[0].numpy()
             
