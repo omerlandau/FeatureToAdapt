435e9a72ba1f1ed705dbe0757bb022b688ad6831
omerlandau <omerlan6@gmail.com>
2020-04-21 22:39:38 +0300

    testing model adaptation

--- Visualize.py
+++ Visualize.py
@@ -11,7 +11,7 @@ from torch.autograd import Variable
 import pickle as pkl
 
 
-def split_all_imgaes(images_p, labels_p, type, direct_l, direct_i, test_adaptation, model_path, gpu0, cropsize):
+def Embedd_Data(images_p, labels_p, type, direct_l, direct_i, test_adaptation, model_path, gpu0, cropsize):
     if(test_adaptation):
         IMG_MEAN = np.array((104.00698793, 116.66876762, 122.67891434), dtype=np.float32)
         model = Res_Deeplab(num_classes=19)
@@ -489,7 +489,7 @@ def main():
                   'munster/munster_000076_000019_gtFine_color.png',
                   'munster/munster_000091_000019_gtFine_color.png']
 
-    gta_images,gta_cmap = split_all_imgaes(gta_ids,gta_ids,'GTA',direct_i='./data/GTA5/images', direct_l='./data/GTA5/labels', test_adaptation=True, model_path='./snapshots/GTA2Cityscapes_norm_00015_Damping15_normal_weight_loss_restore_from_40000_G_38_D_numsteps_fixed/GTA5_40000.pth', gpu0=3, cropsize=(1280,720))
+    gta_images,gta_cmap = Embedd_Data(gta_ids,gta_ids,'GTA',direct_i='./data/GTA5/images', direct_l='./data/GTA5/labels', test_adaptation=True, model_path='./snapshots/GTA2Cityscapes_norm_00015_Damping15_normal_weight_loss_restore_from_40000_G_38_D_numsteps_fixed/GTA5_40000.pth', gpu0=3, cropsize=(1280,720))
 
 
     with open("./Adapted_GTA_p20_exagg", 'wb') as pfile:
@@ -500,7 +500,7 @@ def main():
 
     print('Dumped GTA pickle')
 
-    city_images, city_cmap = split_all_imgaes(city_ids_i,city_ids_l,type='city', direct_l='./data/CitySpaces/gtFine/val', direct_i='./data/CitySpaces/leftImg8bit/val', test_adaptation=True, model_path='./snapshots/GTA2Cityscapes_norm_00015_Damping15_normal_weight_loss_restore_from_40000_G_38_D_numsteps_fixed/GTA5_40000.pth', gpu0=1, cropsize=(1024,512))
+    city_images, city_cmap = Embedd_Data(city_ids_i,city_ids_l,type='city', direct_l='./data/CitySpaces/gtFine/val', direct_i='./data/CitySpaces/leftImg8bit/val', test_adaptation=True, model_path='./snapshots/GTA2Cityscapes_norm_00015_Damping15_normal_weight_loss_restore_from_40000_G_38_D_numsteps_fixed/GTA5_40000.pth', gpu0=1, cropsize=(1024,512))
 
 
     with open("./Adapted_City_p20_exagg", 'wb') as pfile:
