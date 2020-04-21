8f8876d71fdf878d7360461269f08be4085b2d12
omerlandau <omerlan6@gmail.com>
2020-04-21 22:28:57 +0300

    testing model adaptation

--- Visualize.py
+++ Visualize.py
@@ -60,16 +60,17 @@ def split_all_imgaes(images_p, labels_p, type, direct_l, direct_i, test_adaptati
             imaget = imaget.flatten()
             imaget = imaget.reshape((shape_x, shape_y, 3))
             if(test_adaptation):
-                imaget = np.asarray(imaget, np.uint8)
-                imaget = Image.fromarray(imaget)
-                imaget = imaget.resize(cropsize, Image.BICUBIC)
-                imaget = np.asarray(imaget, np.float32)
-                imaget = imaget[:, :, ::-1]  # change to BGR
-                #imaget -= IMG_MEAN
-                imaget = imaget.transpose((2, 0, 1))
-                print(imaget)
-                _, _, imaget = model(Variable(torch.unsqueeze(torch.from_numpy(imaget.copy()),dim=0)).cuda(gpu0))
-                imaget = imaget.data.cpu().numpy()
+                with torch.no_grad():
+                    imaget = np.asarray(imaget, np.uint8)
+                    imaget = Image.fromarray(imaget)
+                    imaget = imaget.resize(cropsize, Image.BICUBIC)
+                    imaget = np.asarray(imaget, np.float32)
+                    imaget = imaget[:, :, ::-1]  # change to BGR
+                    #imaget -= IMG_MEAN
+                    imaget = imaget.transpose((2, 0, 1))
+                    print(imaget)
+                    _, _, imaget = model(Variable(torch.unsqueeze(torch.from_numpy(imaget.copy()),dim=0)).cuda(gpu0))
+                    imaget = imaget.data.cpu().numpy()
             else:
                 imaget = imaget.reshape((shape_x, shape_y * 3))
             print(imaget.shape)
