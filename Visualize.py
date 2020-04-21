132f653cc1b8fc95574f597ed2c8898d2c3fe66c
omerlandau <omerlan6@gmail.com>
2020-04-21 21:21:26 +0300

    testing model adaptation

--- Visualize.py
+++ Visualize.py
@@ -13,7 +13,7 @@ import pickle as pkl
 def split_all_imgaes(images_p, labels_p, type, direct_l, direct_i, test_adaptation, model_path, gpu0):
     if(test_adaptation):
         model = Res_Deeplab(num_classes=args.num_classes)
-        saved_state_dict = torch.load(args.restore_from, map_location="cuda:{0}".format(gpu0))
+        saved_state_dict = torch.load(model_path, map_location="cuda:{0}".format(gpu0))
         model.load_state_dict(saved_state_dict)
         model.eval()
         model.cuda(gpu0)
